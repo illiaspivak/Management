2a26c9f184a45dae6a2df00e04149af60d5dc203
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-03-05 09:51:43 +0100

    Method getCurrentTime

--- src/main/java/sk/kosickaakademia/illiaspivak/company/App.java
+++ src/main/java/sk/kosickaakademia/illiaspivak/company/App.java
@@ -1,10 +1,11 @@
 package sk.kosickaakademia.illiaspivak.company;
 
 import sk.kosickaakademia.illiaspivak.company.database.Database;
+import sk.kosickaakademia.illiaspivak.company.util.Util;
 
 public class App {
     public static void main(String[] args) {
-        Database database=new Database();
-        database.getConnection();
+        Util util = new Util();
+        System.out.println(util.getCurrentTime());
     }
 }
--- src/main/java/sk/kosickaakademia/illiaspivak/company/util/Util.java
+++ src/main/java/sk/kosickaakademia/illiaspivak/company/util/Util.java
@@ -2,6 +2,8 @@ package sk.kosickaakademia.illiaspivak.company.util;
 
 import sk.kosickaakademia.illiaspivak.company.entity.User;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Util {
@@ -17,8 +19,9 @@ public class Util {
     }
 
     public String getCurrentTime(){
-
-        return null;
+        Date dateNow = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        return formatForDateNow.format(dateNow);
     }
 
     public String normalizeName (String name){
